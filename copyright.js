0a5f843b4c6be7113949f6f2ee3540c613ee6dd3
Jim Allen <iggalot.iggy@gmail.com>
2019-09-22 22:37:58 -0500

    Initial backup of EOC website from 9/19/2019.

---
+++ copyright.js
@@ -0,0 +1,10 @@
+{
+document.write('<span');
+document.write(' style="color: #FFF;">&nbsp; &nbsp;&nbsp;');
+document.write('<small>Copyright 2012 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; <a style="color:#FFF; text-decoration: none;" href="mailto:webmanager@evansvilleobedienceclub.com">Contact Webmaster</a></small>');
+document.write('</span>');
+
+}
+
+
+
